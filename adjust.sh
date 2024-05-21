d87eb46b4ebfe503949bcb306ad68ccd3429e454
Alam Putra <69661788+Alamputraaf@users.noreply.github.com>
2024-05-21 19:19:56 +0700

    Create adjust.sh

---
+++ adjust.sh
@@ -0,0 +1,8 @@
+##Gentoo Wiki
+https://wiki.gentoo.org/wiki/Power_management/Processor#cite_note-cppc-12
+
+##Adjust Frequency
+sudo nano /sys/devices/system/cpu/cpu1/cpufreq/scaling_max_freq 
+
+##Disable Boost
+sudo nano /sys/devices/system/cpu/cpufreq/boost 
